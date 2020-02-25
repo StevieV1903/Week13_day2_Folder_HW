a978dc9e32ee7cd30af2129b71f83096fc11eeda
Steve Vance <steve.vance@hotmail.co.uk>
2020-02-25 15:51:44 +0000

    first init

---
+++ src/main/java/com/codeclan/example/FolderLabHomework/FolderLabHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.FolderLabHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderLabHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderLabHomeworkApplication.class, args);
+	}
+
+}
